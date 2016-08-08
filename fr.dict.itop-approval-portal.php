a2cd07ab12e820ff21db8384fc0834f7656542e5
Romain Quetiez <romain.quetiez@combodo.com>
2016-08-08 15:35:13 +0000

    Module to approve user request from the portal (requires approval-base 2.4.0)
    SVN:trunk[5453]

---
+++ fr.dict.itop-approval-portal.php
@@ -0,0 +1,25 @@
+<?php
+// Copyright (C) 2012 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	'Approval:Portal:Menu' => 'Vos approbations',
+	'Approval:Portal:Menu+' => 'Demandes pour lesquelles votre approbation est requise',
+	'Approval:Portal:Title' => 'Demandes en attente de votre approbation',
+	'Approval:Portal:Title+' => 'Veuillez sélectionner les demandes à approuver, puis utiliser les boutons au bas de la page.',
+	'Approval:Portal:NoItem' => 'Il n\'y a pas de demande en attente de votre approbation',
+	'Approval:Portal:Dlg:Approve' => 'Veuillez confirmer votre approbation pour <em><span id="approval-count">?</span></em> demande(s)',
+	'Approval:Portal:Dlg:Reject' => 'Veuillez confirmer votre refus pour <em><span id="approval-count">?</span></em> demande(s)',
+));
