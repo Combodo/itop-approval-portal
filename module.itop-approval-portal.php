14ceb23e77c78979e74f4673b8325abc5bf07b33
Denis Flaven <denis.flaven@combodo.com>
2018-01-26 15:51:31 +0000

    Version 1.0.1: this module is automatically selected only if the Enhanced Portal is installed.
    SVN:1.0.1[6444]

--- module.itop-approval-portal.php
+++ module.itop-approval-portal.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-approval-portal/1.0.0',
+	'itop-approval-portal/1.0.1',
 	array(
 		// Identification
 		//
@@ -19,7 +19,8 @@ SetupWebPage::AddModule(
 			'approval-base/2.4.1'
 		),
 		'mandatory' => false,
-		'visible' => true,
+		'visible' => false,
+		'auto_select' => 'SetupInfo::ModuleIsSelected("itop-portal")',
 
 		// Components
 		//
