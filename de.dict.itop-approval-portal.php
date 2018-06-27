dd70cf1a996feccff501b93ebc9f2db00771fdf7
Romain Quetiez <romain.quetiez@combodo.com>
2018-06-27 15:09:39 +0000

    added german translation (itomig)
    SVN:trunk[6833]

---
+++ de.dict.itop-approval-portal.php
@@ -0,0 +1,35 @@
+
+<?php
+// Copyright (C) 2012 Combodo SARL
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License as published by
+//   the Free Software Foundation; version 3 of the License.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   along with this program; if not, write to the Free Software
+//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+Dict::Add('DE DE', 'German', 'Deutsch', array(
+	'Approval:Portal:Menu' => 'Genehmigungen',
+	'Approval:Portal:Menu+' => 'Auf Ihre Freigabe wartende Anfragen',
+	'Approval:Portal:Title' => 'Auf Ihre Freigabe wartende Anfragen',
+	'Approval:Portal:Title+' => 'Bitte wählen Sie die Anfragen aus und nutzen Sie die Buttons unten um zu genehmigen oder abzulehen.',
+	'Approval:Portal:NoItem' => 'Aktuell gibt es keine Anfragen, die auf Ihre Freigabe warten.',
+	'Approval:Portal:Dlg:Approve' => 'Bitte bestätigten Sie, dass Sie <em><span class="approval-count">?</span></em> Anfragen genehmigen wollen.',
+	'Approval:Portal:Dlg:Reject' => 'Bitte bestätigten Sie, dass Sie <em><span class="approval-count">?</span></em> Anfragen zurückweisen wollen.',
+	'Approval:Portal:Dlg:ApproveOne' => 'Bitte bestätigten Sie, um die Anfrage zu genehmigen.',
+	'Approval:Portal:Dlg:RejectOne' => 'Bitte bestätigten Sie, um diese Anfrage zurückzuweisen.',
+	'Approval:Portal:Dlg:Btn:Approve' => 'Genehmigen',
+	'Approval:Portal:Dlg:Btn:Reject' => 'Ablehnen',
+	'Approval:Portal:Btn:Approve' => 'Genehmigen',
+	'Approval:Portal:Btn:Reject' => 'Ablehnen',
+	'Approval:Portal:CommentTitle' => 'Kommentar (zwigend erforderlich im Falle einer Ablehnung)',
+	'Approval:Portal:CommentPlaceholder' => '',
+	'Approval:Portal:Success' => 'Ihre Rückmeldung wurde verarbeitet.',
+));
