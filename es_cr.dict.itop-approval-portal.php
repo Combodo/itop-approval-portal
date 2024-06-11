f6c6a4082447a0bfeadf16dc8f798918fc07dae9
Timothee <timothee.callet@combodo.com>
2024-06-11 16:59:00 +0200

    Automated dictionaries synchronization with english reference

--- es_cr.dict.itop-approval-portal.php
+++ es_cr.dict.itop-approval-portal.php
@@ -1,33 +1,23 @@
 <?php
-// Copyright (C) 2012 Combodo SARL
-//
-//   This program is free software; you can redistribute it and/or modify
-//   it under the terms of the GNU General Public License as published by
-//   the Free Software Foundation; version 3 of the License.
-//
-//   This program is distributed in the hope that it will be useful,
-//   but WITHOUT ANY WARRANTY; without even the implied warranty of
-//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//   GNU General Public License for more details.
-//
-//   You should have received a copy of the GNU General Public License
-//   along with this program; if not, write to the Free Software
-//   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- /**
- * Spanish Localized data
+/**
+ * Localized data
  *
- * @copyright   Copyright (C) 2010-2024 Combodo SAS
- * @license     http://opensource.org/licenses/AGPL-3.0
- * @traductor   Miguel Turrubiates <miguel_tf@yahoo.com>
+ * @copyright Copyright (C) 2010-2024 Combodo SAS
+ * @license    https://opensource.org/licenses/AGPL-3.0
+ * 
  */
-Dict::Add('ES CR', 'Spanish', 'Español, Castellano', array(
+/**
+ * @author Miguel Turrubiates <miguel_tf@yahoo.com>
+ *
+ */
+Dict::Add('ES CR', 'Spanish', 'Español, Castellano', [
+	'Approval:Portal:Dlg:Approve' => 'Por favor confirme que desea aprobar <em><span class="approval-count">?</span></em> requerimientos',
+	'Approval:Portal:Dlg:ApproveOne' => 'Por favor confirme que desea aprobar este requerimiento',
+	'Approval:Portal:Dlg:Reject' => 'Por favor confirme que desea rechazar <em><span class="approval-count">?</span></em> requerimientos',
+	'Approval:Portal:Dlg:RejectOne' => 'Por favor confirme que desea rechazar este requerimiento',
 	'Approval:Portal:Menu' => 'Aprobaciones',
 	'Approval:Portal:Menu+' => 'Requerimientos para los cuales su aprobación es esperada',
+	'Approval:Portal:NoItem' => 'No hay requerimientos esperando su aprobación',
 	'Approval:Portal:Title' => 'Requerimientos esperando su aprobación',
 	'Approval:Portal:Title+' => 'Por favor seleccione los requerimientos a aprobar y utilice los botones localizados en la parte inferior de la página.',
-	'Approval:Portal:NoItem' => 'No hay requerimientos esperando su aprobación',
-	'Approval:Portal:Dlg:Approve' => 'Por favor confirme que desea aprobar <em><span class="approval-count">?</span></em> requerimientos',
-	'Approval:Portal:Dlg:Reject' => 'Por favor confirme que desea rechazar <em><span class="approval-count">?</span></em> requerimientos',
-	'Approval:Portal:Dlg:ApproveOne' => 'Por favor confirme que desea aprobar este requerimiento',
-	'Approval:Portal:Dlg:RejectOne' => 'Por favor confirme que desea rechazar este requerimiento',
-));
+]);
