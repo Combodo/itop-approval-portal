2217ad4a1c06fe3708053e6e597d468ba10681f5
Anne-Cath <anne-catherine.cognet@combodo.com>
2026-02-11 09:54:31 +0100

    Change version number to 1.0.12

--- module.itop-approval-portal.php
+++ module.itop-approval-portal.php
@@ -5,7 +5,7 @@
 
 SetupWebPage::AddModule(
 	__FILE__, // Path to the current file, all other file names are relative to the directory containing this file
-	'itop-approval-portal/1.0.11',
+	'itop-approval-portal/1.0.12',
 	array(
 		// Identification
 		//
